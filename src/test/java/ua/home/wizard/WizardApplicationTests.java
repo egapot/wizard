cfc78234cb2a21762f74953a2b4024e6e840f8d6
egapot <57811970+egapot@users.noreply.github.com>
2020-06-16 13:13:02 +0300

    Initial commit

---
+++ src/test/java/ua/home/wizard/WizardApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.home.wizard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WizardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
