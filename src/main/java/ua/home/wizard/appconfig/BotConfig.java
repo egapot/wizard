02cbd5acbfd1cb9276b1c5dc821e21ad0407a967
egapot <57811970+egapot@users.noreply.github.com>
2020-06-17 08:25:49 +0300

    echo version

---
+++ src/main/java/ua/home/wizard/appconfig/BotConfig.java
@@ -0,0 +1,44 @@
+package ua.home.wizard.appconfig;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.telegram.telegrambots.bots.DefaultBotOptions;
+import org.telegram.telegrambots.meta.ApiContext;
+import ua.home.wizard.MyWizardTelegramBot;
+
+@Setter
+@Getter
+@Configuration
+@ConfigurationProperties(prefix = "telegrambot")
+public class BotConfig {
+    private String webHookPath;
+    private String botUserName;
+    private String botToken;
+
+    @Bean
+    public MyWizardTelegramBot myWizardTelegramBot () {
+        DefaultBotOptions options = ApiContext.getInstance(DefaultBotOptions.class);
+
+        MyWizardTelegramBot myWizardTelegramBot = new MyWizardTelegramBot(options);
+        myWizardTelegramBot.setBotUserName(botUserName);
+        myWizardTelegramBot.setBotToken(botToken);
+        myWizardTelegramBot.setWebHookPath(webHookPath);
+
+        return  myWizardTelegramBot;
+    }
+
+    @Bean
+    public MessageSource messageSource() {
+        ReloadableResourceBundleMessageSource messageSource
+                = new ReloadableResourceBundleMessageSource();
+
+        messageSource.setBasename("classpath:messages");
+        messageSource.setDefaultEncoding("UTF-8");
+        return  messageSource;
+    }
+}
